c4f22227a4384cb30f4aa8339e61abeaf2ba0581
Vineetha Veerapu <vineethaveerapu4@gmail.com>
2025-10-05 14:15:53 +0200

    finish ReadingListContext.test, ReadingListPage.test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@
 
 import BookList from '@/components/BookList';
 import { books } from '@/data/books';
+import { useReadingList } from '@/components/ReadingListProvider';
 
 export default function Home() {
+  const { addBook } = useReadingList();
+
   const handleAdd = (title: string) => {
-    console.log(title);
+    addBook(title);
   };
+
   return (
     <div>
       <BookList
